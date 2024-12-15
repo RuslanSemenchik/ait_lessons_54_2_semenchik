394b6f44fbe29cd5fd058daf6f98125b19524270
Ruslan Semenchik <semenchik.r@gmail.com>
2024-12-15 23:12:22 +0100

    initial commit

---
+++ src/main/java/de/ait/Main.java
@@ -0,0 +1,10 @@
+package de.ait;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    public static String getGreeting (String name){
+        return "Hello, "+ name+" !";
+    }
+}
