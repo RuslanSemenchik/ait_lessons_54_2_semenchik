ff29107260dd89dd189a70b5528eb1b4395a25c4
Ruslan Semenchik <semenchik.r@gmail.com>
2024-12-16 00:32:31 +0100

    #1:Unit-Test was implemented

--- src/main/java/de/ait/Main.java
+++ src/main/java/de/ait/Main.java
@@ -1,10 +1,9 @@
 package de.ait;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+
     public static String getGreeting (String name){
-        return "Hello, "+ name+" !";
+        return "Hello, "+ name+"!";
     }
 }
